2382a172ae924cbb6e549e7ef05a13e16c59bf2c
tresnax <tresna.widiyaman@gmail.com>
2021-07-13 16:37:00 +0700

    Menambahkan wordpress data_option

--- fungsi.php
+++ fungsi.php
@@ -47,8 +47,33 @@ function fungsi_testmenu(){
 }
 
 function fungsi_submenu(){
-    echo '<h2>Setting Plugin sub menu</h2>
-    <p> Ini adalah fasilitas dari sub menu</p>';
+    if ($_POST['nohp'] != '') {
+        update_option('wa_nohp', $_POST['nohp']);
+        update_option('wa_pesan', $_POST['pesan']);
+        update_option('wa_text', $_POST['text']);
+        echo 'Data berhasil di Update';
+    }
+    
+    echo '<h2>Pengaturan Data Whatsapp</h2>
+    <p>Silahkan mengisi informasi dibawah ini untuk melakukan costume WA Apps</p>
+    <form action="" method="post">
+    <table border=0>
+    <tr>
+        <td>Nomor Whatsapp  : </td>
+        <td><input type="text" name="nohp" value="'.get_option('wa_nohp').'"/></td>
+    </tr><tr>
+        <td>Pesan Default   : </td>
+        <td><input type="text" name="pesan" value="'.get_option('wa_pesan').'"/></td>
+    </tr><tr>
+        <td>Text            : </td>
+        <td><input type="text" name="text" value="'.get_option('wa_text').'"/></td>
+    </tr><tr>
+        <td><input type="submit" value="Update"/></td>
+        <td></td>
+    </tr>
+    </table>
+    </form>
+    ';
 }
 
 add_action('admin_menu','testmenuadmin'); //Memasukan fungsi yang sudah dibuat ke wordpress
@@ -60,11 +85,14 @@ function fungsi_iklan () {
 add_shortcode('iklan','fungsi_iklan'); //Memasukan fungsi shortcode ke wordpress dengan code 'iklan'
 
 function fungsi_whatsapp ($atts) {
+    $nohp = get_option('wa_nohp');
+    $pesan = get_option('wa_pesan');
+    $text = get_option('wa_text');
     $var = shortcode_atts(
         array(
-            'nohp' => '6285722994333',
-            'pesan' => 'Mohon info beli ikan cupang',
-            'text' => 'Pesan disini'
+            'nohp' => $nohp,
+            'pesan' => $pesan,
+            'text' => $text
         ),$atts 
     );
 
--- readme.md
+++ readme.md
@@ -1,3 +1,11 @@
 # Wordpress Puligin Basic
 
 Ini adalah sebuah plugin basic wordpress, disini akan dijelaskan bagaimana cara membuat sebuah plugin wordpress sederhana mulai dari a-z
+
+## Update & Get Option
+
+Di wordpress kita bisa menyimpan beberapa variabel yang bersifat dinamis dan tersimpan di database, jadi misalkan kita membuat form untuk menyimpan nomer hp, tapi data ini akan ditimpa apabila kita edit datanya. Kita bisa menggunakan update option seperti contoh berikut :
+[shortcode] update_option('wa_nohp', $_POST['nohp']);[/shortcode]
+
+Nanti untuk bisa mendapatkan kembali value yang sudah kita simpan dari update option, kita bisa lakukan get option dengan perintah berikut :
+[shortcode] get_option('wa_nohp'); [/shortcode]
